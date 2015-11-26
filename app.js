bd14c2dbadddbdc89d55e16529f1f025dc5638cc
Wallysson Nunes <wally.nm@gmail.com>
2015-11-26 12:50:24 -0200

    Twitter nodejs update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var qs      = require('querystring');
 var express = require('express');
 var cors    = require('cors');
 var request = require('request');
@@ -5,14 +6,19 @@ var storage = require('node-persist');
 var raven   = require('raven');
 var _       = require('lodash');
 var Q       = require('q');
-
-console.log('App started');
+var shortid = require('shortid');
+var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
+var db      = mongojs('connect');
+var authCollection = db.collection('userauth');
 
 // Modules
 var twitterAPI = require('./modules/twitter');
 var app = express();
 app.use(cors({origin: '*'}));
 app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 storage.init({
   dir:__dirname+'/store',
@@ -22,7 +28,7 @@ app.get('/twitter/trend', function(req, res) {
   console.log('app route -> /twitter/trend');
   var placeID = res.get('id') || 1;
 
-  twitterAPI.getTokenApp()
+  twitterAPI.getTokenOauth()
   .then(function(oauthRes){
     return twitterAPI.getTrendingTopics(oauthRes, placeID);
   })
@@ -36,11 +42,14 @@ app.get('/twitter/search/tweets', function(req, res) {
   var placeID = res.get('id') || 1;
   var oauthRes = undefined;
 
-  twitterAPI.getTokenApp().then(function(auth){
+  var test = {
+    token: "197243204-dSpcZmy6sLmtb2UeN40pkcxiT6DK43H36UYEKDN1"
+  };
 
+  twitterAPI.getTokenOauth(test).then(function(auth){
     twitterAPI.getTrendingTopics(auth, placeID)
     .then(function(trendingTopics) {
-      var trends = trendingTopics[0].trends;
+      var trends = trendingTopics.trends;
       var placeID = res.get('id') || 1;
       var query = '';
 
@@ -50,68 +59,68 @@ app.get('/twitter/search/tweets', function(req, res) {
         if(trends.length -1 != i) query += ' OR ';
       });
 
-      twitterAPI.queryTweets(auth, placeID, query)
+      twitterAPI.queryTweets(auth, query, placeID)
       .then(function(body){
-        console.log('bodyuuu', body)
         res.send(body);
       });
     });
   });
 });
 
-// app.get('/twitter/auth', function(req, res) {
-//   console.log('app route -> /twitter/search/tweets');
-//   var placeID = res.get('id') || 1;
-//   var oauthRes = undefined;
-
-//   twitterAPI.getTokenApp().then(function(auth){
-
-//     twitterAPI.getTrendingTopics(auth, placeID)
-//     .then(function(trendingTopics) {
-//       var trends = trendingTopics[0].trends;
-//       var placeID = res.get('id') || 1;
-//       var query = '';
-
-//       // Loop trends building query
-//       _.each(trends, function(k, i){
-//         query += k.query;
-//         if(trends.length -1 != i) query += ' OR ';
-//       });
-
-//       twitterAPI.queryTweets(auth, placeID, query)
-//       .then(function(body){
-//         console.log('bodyuuu', body)
-//         res.send(body);
-//       });
-//     });
-//   });
-// });
-
-
-app.get('/twitter/device', function(req, res) {
-  twitterAPI.authRequestPinUrl()
-  .then(function(authPinUrl){
-    res.writeHead(301, {Location: authPinUrl});
-    res.end();
-  });
-})
-
-
-
-
-
 /**
- * Method used to control error handling
+ * Generate URL Auth token and redirects user to Twitter PIN Page
  */
-function onError(err, req, res, next) {
-  res.statusCode = 500;
-  res.end(res.sentry+'\n');
-}
+app.get('/twitter/device/:key?', function(req, res) {
+  var key = req.params.key;
 
-// Ravenjs configure
-app.use(raven.middleware.express.requestHandler('https://43d873a54a3a4fdbbcc4f23a9c2bd5c9:7f90d13250294e4a8c2ab5df9e2e8a29@app.getsentry.com/59248'));
-app.use(raven.middleware.express.errorHandler('https://43d873a54a3a4fdbbcc4f23a9c2bd5c9:7f90d13250294e4a8c2ab5df9e2e8a29@app.getsentry.com/59248'));
-app.use(onError);
+  if(_.isUndefined(key)){
+    res.send('Oops. Parece que você não informou a url completa, tente verificar se a url está igual a exibida na sua TV LG.');
+    return;
+  }
+
+  authCollection.findOne({shortkey: key}, function(err, doc) {
+    if(_.isUndefined(doc)) {
+      res.send('Oops. Confira se os ultimos valores da URL informada estão certos, caso sejam iguais tente acessar novamente a tela de login na sua TV LG.');
+      return;
+    }
+
+    res.redirect('https://api.twitter.com/oauth/authenticate' + '?' + qs.stringify({oauth_token: doc.oauth_token}));
+  });
+});
+
+
+// Gera a token no server e prepara os 
+// dados para validar a token do usuario
+app.get('/twitter/url_device', function(req, res) {
+  twitterAPI.authRequestPinUrl()
+  .then(function(result) {
+    res.send(result);
+  });
+});
+
+// Gera a token no server e prepara os 
+app.post('/twitter/device/auth', function(req, res) {
+  twitterAPI.authPinUser(req.body)
+  .then(function(result) {
+    res.send(result);
+  });
+});
+
+
+
+
+// /**
+//  * Method used to control error handling
+//  */
+// function onError(err, req, res, next) {
+//   res.statusCode = 500;
+//   res.end(res.sentry+'\n');
+// }
+
+// // Ravenjs configure
+// app.use(raven.middleware.express.requestHandler('https://43d873a54a3a4fdbbcc4f23a9c2bd5c9:7f90d13250294e4a8c2ab5df9e2e8a29@app.getsentry.com/59248'));
+// app.use(raven.middleware.express.errorHandler('https://43d873a54a3a4fdbbcc4f23a9c2bd5c9:7f90d13250294e4a8c2ab5df9e2e8a29@app.getsentry.com/59248'));
+// app.use(onError);
 
 // App listening mode
 app.listen(3000);
--- modules/twitter.js
+++ modules/twitter.js
@@ -2,12 +2,30 @@ var qs = require('querystring');
 var express = require('express');
 var request = require('request');
 var storage = require('node-persist');
-var raven   = require('raven');
-var _       = require('lodash');
-var Q       = require('q');
+var raven = require('raven');
+var _ = require('lodash');
+var Q = require('q');
+var shortid = require('shortid');
+
+var mongojs = require('mongojs');
+var db = mongojs('connect');
+var Twit = require('twit');
+
+var authCollection = db.collection('userauth');
+var authBulk = authCollection.initializeOrderedBulkOp();
+var trendsCollection = db.collection('trends');
+var trendsBulk = trendsCollection.initializeOrderedBulkOp();
+var tweetsCollection = db.collection('tweets');
+var tweetsBulk = tweetsCollection.initializeOrderedBulkOp();
+
+
+// Find all documents created after midnight on May 25th, 1980
+// db.mycollection.find({ _id: { $gt: helper.objectIdWithTimestamp('1980/05/25') } });
+// db.requests.find({timestamp: {$lt: new Date((new Date())-1000*60*60*72)}}).count()
+
 
 storage.init({
-  dir:'../store',
+  dir: '../store',
   stringify: JSON.stringify,
   parse: JSON.parse,
   encoding: 'utf8',
@@ -18,8 +36,15 @@ var config = {
   "consumerSecret": "L29qez89jTSet6NzEuo0iqi6UvmvgPOnyqG5LDSj36sNR8iQDs",
 }
 
- //Get this data from your twitter apps dashboard
-exports.config;
+// var T = new Twit({
+//   consumer_key:         '...', 
+//   consumer_secret:      '...', 
+//   access_token:          '...', 
+//   access_token_secret:  '...'
+// })
+
+//Get this data from your twitter apps dashboard
+exports.config = config;
 
 var twitterAppKey = new Buffer(encodeURIComponent(config.consumerKey) + ':' + encodeURIComponent(config.consumerSecret)).toString('base64');
 
@@ -27,9 +52,13 @@ var twitterAppKey = new Buffer(encodeURIComponent(config.consumerKey) + ':' + en
  * Auths the app to make the basic requests and stores basic token for control in app
  * @return {[type]}            [description]
  */
-exports.getTokenApp = function() {
+exports.getTokenOauth = function(parameters) {
   var defer = Q.defer();
   var oauthRes = storage.getItem('twitterAppOauthResponse');
+
+  if(!_.isUndefined(parameters) && !_.isEmpty(parameters)) {
+    defer.resolve(parameters);
+  }
 
   if (_.isUndefined(oauthRes) || _.isEmpty(oauthRes)) {
     request.post({
@@ -59,106 +88,167 @@ exports.getTokenApp = function() {
 
 exports.getTrendingTopics = function(oauthRes, placeID) {
   var defer = Q.defer();
-  var storageKey = 'twitter_trending_' + placeID;
-  var trending = storage.getItem(storageKey);
+  var date = getPastMinutes(10);
+  clearOldTrends(date);
 
-  if (_.isUndefined(trending) || _.isEmpty(trending)) {
+  trendsCollection.findOne({timestamp: {$gt: date}, 'locations.0.woeid': placeID}, function(err, doc) {
+    if (_.isUndefined(doc) || _.isEmpty(doc)) {
 
-    request.get({
-      url: 'https://api.twitter.com/1.1/trends/place.json',
-      headers: {
-        'Authorization': _.capitalize(oauthRes.token_type) + ' ' + oauthRes.access_token,
-      },
-      qs: {
-        id: placeID
-      },
-    },
-    function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        trending = JSON.parse(body);
-        trending[0].timestamp = new Date();
-        storage.setItem(storageKey, trending);
-        defer.resolve(trending);
+      oauthRes.consumer_key = config.consumerKey;
+      var requestObject = {
+        url: 'https://api.twitter.com/1.1/trends/place.json',
+        qs: {
+          id: placeID
+        }
+      };
+
+      if(oauthRes.token_type === 'bearer') {
+        requestObject.headers =  {'Authorization': _.capitalize(oauthRes.token_type) + ' ' + oauthRes.access_token};
       } else {
-        throw new Error(body);
-        defer.reject();
+        requestObject.headers =  {
+          'Authorion': 'OAuth oauth_consumer_key="vZPfoRahVcPOPKNtzHRjqeQwZ", oauth_nonce="d003540bb9afc68c07e87907d200509b", oauth_signature="LQHKImuWquH%2BT1Lzv9LHjeFXPas%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1448546794", oauth_token="197243204-dSpcZmy6sLmtb2UeN40pkcxiT6DK43H36UYEKDN1", oauth_version="1.0"'
+        };
+        // requestObject.oauth = oauthRes;
       }
-    });
-  } else {
-    defer.resolve(trending);
-  }
 
+      request.get(requestObject, function(error, response, body) {
+        console.warn(response)
+        console.warn(requestObject)
+        console.warn(error)
+
+
+        if (!error && response.statusCode == 200) {
+          var object = JSON.parse(body)[0];
+          object.timestamp = new Date();
+          trendsCollection.insert(object);
+          defer.resolve(object);
+        } else {
+          throw new Error(body);
+          defer.reject();
+        }
+      });
+    } else {
+      defer.resolve(doc);
+    }
+  });
   return defer.promise;
 }
 
-exports.queryTweets = function(oauthRes, placeID, query) {
+exports.queryTweets = function(oauthRes, query, placeID) {
   var defer = Q.defer();
-  var storageKey = 'twitter_search_' + placeID;
-  var tweets = storage.getItem(storageKey);
+  var date = getPastMinutes(1);
+  clearOldTweets(date);
 
-  if (_.isUndefined(tweets) || _.isEmpty(tweets)) {
-    request.get({
-      url: 'https://api.twitter.com/1.1/search/tweets.json',
-      headers: {
-        'Authorization': _.capitalize(oauthRes.token_type) + ' ' + oauthRes.access_token,
-      },
-      qs: {
-        q: query
-      }
-    }, 
-    function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        tweets = JSON.parse(body);
-        tweets.timestamp = new Date();
-        storage.setItem(storageKey, tweets);        
-        defer.resolve(tweets);
+  tweetsCollection.findOne({timestamp: {$gt: date}}, function(err, doc) {
+    if (_.isUndefined(doc) || _.isEmpty(doc)) {
+
+      oauthRes.consumer_key = config.consumerKey;
+      var requestObject = {
+        url: 'https://api.twitter.com/1.1/search/tweets.json',
+        qs: {
+          count: 100,
+          q: query
+        }
+      };
+
+      if(oauthRes.token_type === 'bearer') {
+        requestObject.headers =  {'Authorization': _.capitalize(oauthRes.token_type) + ' ' + oauthRes.access_token};
       } else {
-        throw new Error(body);
+        requestObject.headers =  {'Authorization': 'OAuth oauth_consumer_key="vZPfoRahVcPOPKNtzHRjqeQwZ", oauth_nonce="d003540bb9afc68c07e87907d200509b", oauth_signature="LQHKImuWquH%2BT1Lzv9LHjeFXPas%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1448546794", oauth_token="197243204-dSpcZmy6sLmtb2UeN40pkcxiT6DK43H36UYEKDN1", oauth_version="1.0"'};
+        // requestObject.oauth = oauthRes;
       }
-    });
-  } else {
-    defer.resolve(tweets);
-  }
 
-  return defer.promise;
-}
+      request.get(requestObject, function(error, response, body) {
+        console.warn(response)
+        if (!error && response.statusCode == 200) {
+          tweets = JSON.parse(body);
+          tweets.timestamp = new Date();
+          tweetsCollection.insert(tweets);
+          defer.resolve(tweets);
+        } else {
+          throw new Error(body);
+        }
+      });
 
-exports.authRequestPinUrl = function() {
-  var defer = Q.defer();
-  var oauth = { 
-    callback: 'oob', 
-    consumer_key: config.consumerKey, 
-    consumer_secret: config.consumerSecret
-  }
-
-  request.post({url:'https://api.twitter.com/oauth/request_token', oauth:oauth}, function (e, r, body) {
-    var req_data = qs.parse(body)
-    var uri = 'https://api.twitter.com/oauth/authenticate' + '?' + qs.stringify({oauth_token: req_data.oauth_token})
-    defer.resolve(uri);
-
-
-    // // request.get({url: uri}, function(e, r, body){
-    // //   defer.resolve(body);
-    // // })
-
-
-    // // step 3
-    // // after the user is redirected back to your server
-    // var auth_data = qs.parse(body)
-    //   , oauth =
-    //     { consumer_key: config.consumerKey
-    //     , consumer_secret: config.consumerSecret
-    //     , token: auth_data.oauth_token
-    //     , token_secret: req_data.oauth_token_secret
-    //     // , verifier: auth_data.oauth_verifier
-    //     }
-    //   , url = 'https://api.twitter.com/oauth/access_token'
-    //   ;
-    // request.post({url:url, oauth:oauth}, function (e, r, body) {
-    //   // console.log(body)
-    //   // defer.resolve(body);
-    // })
+    } else {
+      // Return the cached value
+      defer.resolve(doc);
+    }
   });
 
   return defer.promise;
+}
+
+
+/**
+ * Generates PIN Url
+ */
+exports.authRequestPinUrl = function() {
+  var defer = Q.defer();
+  clearOldKeys();
+  var oauth = {
+    callback: 'oob',
+    consumer_key: config.consumerKey,
+    consumer_secret: config.consumerSecret
+  }
+
+  request.post({url: 'https://api.twitter.com/oauth/request_token', oauth: oauth}, function(e, r, body) {
+    var req_data = qs.parse(body);
+
+    var object = {
+      oauth_token : req_data.oauth_token,
+      oauth_token_secret : req_data.oauth_token_secret,
+      shortkey: shortid.generate().toUpperCase(),
+      timestamp: new Date(),
+      type: 'twitter'
+    };
+
+    authCollection.insert(object);
+    defer.resolve(object);
+  });
+
+  return defer.promise;
+}
+
+/**
+ * Receives user PIN data and auth the system
+ */
+exports.authPinUser = function(data) {
+  var defer = Q.defer();
+
+  var oauth = {
+    consumer_key: config.consumerKey,
+    consumer_secret: config.consumerSecret,
+    token: data.oauth_token,
+    token_secret: data.oauth_token_secret,
+    verifier: data.PIN
+  };
+
+  request.post({url: 'https://api.twitter.com/oauth/access_token', oauth: oauth}, function(e, r, body) {
+    defer.resolve(qs.parse(body));
+  });
+
+  return defer.promise;
+}
+
+getPastMinutes = function(minutes){
+  var date = new Date();
+  return new Date(date.setMinutes(date.getMinutes() - minutes));
+}
+
+
+clearOldTweets = function(date) {
+  tweetsBulk.find({timestamp: {$lt: date}}).remove();
+  tweetsBulk.execute(function () {});
+}
+
+clearOldTrends = function(date) {
+  // trendsBulk.find({timestamp: {$lt: date}}).remove();
+  // trendsBulk.execute(function () {});
+}
+
+clearOldKeys = function() {
+  var date = getPastMinutes(5);
+  authBulk.find({timestamp: {$lt:date}}).remove();
+  authBulk.execute(function () {});
 }
