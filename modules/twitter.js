e3a89dc84331469caef4b377833c8d7c83b4109e
Wallysson Nunes <wally.nm@gmail.com>
2015-11-30 23:08:54 -0200

    Updated user data

--- modules/twitter.js
+++ modules/twitter.js
@@ -80,8 +80,8 @@ exports.queryTweets = function(query, auth) {
   Tweet.get('search/tweets', query, function(err, data, response){
     if(!_.isUndefined(data.search_metadata)){
       var searchParams = qs.parse(data.search_metadata.next_results.substring(1));
-      console.warn(searchParams)
-      data.max_id = searchParams.max_id;
+      data.search_metadata.max_id = searchParams.max_id;
+      console.warn(data.search_metadata);
     }
 
     defer.resolve(data);
